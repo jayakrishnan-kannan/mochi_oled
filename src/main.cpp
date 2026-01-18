1f40c096cf9556643ded47bcf18257978a2edcf7
jayakrishnan <jayakrishnan@spanidea.com>
2026-01-18 18:18:20 +0530

    Added new animation for home page Added delayy to avoid frame freezing

--- src/main.cpp
+++ src/main.cpp
@@ -27,25 +27,46 @@ void setup(void)
 
 void loop(void)
 {
-  Serial.println("Current Firmware Version: " + String(currentFirmwareVersion));
-	frame++;
-  // run_latest_anime();
-  // delay(1000);
+  // Serial.println("Current Firmware Version: " + String(currentFirmwareVersion));
+	// frame++;
   run_first_smile();
+  run_home_page();
+  run_latest_anime();
+  delay(100);
+  u8g2.clearBuffer();
 
 }
 void run_first_smile(void)
 {
-  	if(frame >= MAX_FRAME){frame = 0;}
-  u8g2.clearBuffer();
-  u8g2.drawXBMP(0,0,128,64, first_smile[frame]);
-  u8g2.sendBuffer();
+  for(frame=0;frame <= MAX_FRAME;frame++)
+  {
+    u8g2.clearBuffer();
+    u8g2.drawXBMP(0,0,128,64, first_smile[frame]);
+    u8g2.sendBuffer();
+    delay(1);
+  }
+  
 
 }
 void run_latest_anime(void)
 {
-  if(frame >= MAX_FRAME_LATEST){frame = 0;}
+  for(frame=0;frame <= MAX_FRAME_LATEST;frame++)
+  {
   u8g2.clearBuffer();
   u8g2.drawXBMP(0,0,128,64, latest_anime_allArray[frame]);
   u8g2.sendBuffer();
+  delay(1);
+
+  }
+}
+
+void run_home_page(void)
+{
+  for(frame=0;frame <= 24;frame++)
+  {
+  u8g2.clearBuffer();
+  u8g2.drawXBMP(0,0,128,64, home_pageallArray[frame]);
+  u8g2.sendBuffer();
+  delay(1);
+  }
 }
--- src/smile_animations.h
+++ src/smile_animations.h
@@ -5,6 +5,8 @@ _Pragma("once")
 #define MAX_FRAME_LATEST 133
 extern const unsigned char* first_smile[MAX_FRAME];
 extern const unsigned char* latest_anime_allArray[MAX_FRAME_LATEST];
+extern const unsigned char* home_pageallArray[25];
 extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2; // initialization for the used OLED display
 void run_latest_anime(void);
 void run_first_smile(void);
+void run_home_page(void);
